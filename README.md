383271b9b3ff95e020cd98ffa912c0357adbf5d0
skirtle <65301168+skirtles-code@users.noreply.github.com>
2025-03-02 16:09:53 +0000

    Reproduction

---
+++ README.md
@@ -0,0 +1,8 @@
+Reproduction for <https://github.com/qmhc/vite-plugin-dts/issues/416>.
+
+```
+npm install
+npm build
+```
+
+Build fails with `vite-plugin-dts@4.5.1`, succeeds with `4.5.0`.
---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import dts from 'vite-plugin-dts'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    dts({
+      rollupTypes: true,
+      tsconfigPath: './tsconfig.app.json'
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'MyLib',
+      fileName: 'my-lib',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
